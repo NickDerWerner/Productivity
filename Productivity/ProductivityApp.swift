b240de5df8059fe0451da16f59787ccafac30af1
nickwerner <ge94nez@tum.de>
2025-09-10 13:11:03 +0200

    Initial Commit

---
+++ Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Nick Werner on 10.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductivityTests/ProductivityTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTests.swift
+//  ProductivityTests
+//
+//  Created by Nick Werner on 10.09.25.
+//
+
+import Testing
+@testable import Productivity
+
+struct ProductivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
